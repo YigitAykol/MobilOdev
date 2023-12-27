05796ba0122974ada07ae809ea958d300e66519d
YigitAykol <61661801+YigitAykol@users.noreply.github.com>
2023-12-27 22:22:45 +0300

    Add files via upload

---
+++ screens/Home.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {Ionicons} from '@expo/vector-icons';
+import sytles from './home.style';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
